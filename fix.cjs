5021f7a4dd74a4de221db909b4528c8d11ea80da
Eduardo <eduardo@local>
2026-09-26 03:35:23 -0400

    chore: limpieza de archivos temporales cjs

--- fix.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/components/AdminPanel.tsx', 'utf8');
-c = c.replace('HelpCircle,\\n  Heart,', 'HelpCircle,\n  Heart,');
-fs.writeFileSync('src/components/AdminPanel.tsx', c, 'utf8');
